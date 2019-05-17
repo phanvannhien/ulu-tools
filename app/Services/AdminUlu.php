ea87e700a93fa1803b59bee7a91bb5d42de26d84
root <root@instance-ulu.asia-east2-a.c.ulu-chinmedia.internal>
2019-05-17 07:37:52 +0000

    add Services folder

---
+++ app/Services/AdminUlu.php
@@ -0,0 +1,49 @@
+<?php
+mespace App\Services;
+use GuzzleHttp\Client;
+
+
+class AdminUlu{
+    protected $baseUrl;
+
+    public function __construct()
+    {
+        if( env('APP_ENV') == 'local'){
+            $this->baseUrl = env('API_LOCAL_BASE_URL');
+        }else{
+            $this->baseUrl = env('API_SERVER_BASE_URL');
+        }
+
+    }
+
+    /**
+     * @param array $params
+     * @return mixed|string
+     * GET traffic 
+     */
+    public function getTraffic( $params = array() ){
+        $url = $this->baseUrl.'/api/admin/traffic';
+        $client = new Client();
+        $response =  $client->request('GET', $url, [
+            'query' => $params
+        ]);
+        $data = $response->getBody()->getContents();
+        $data = json_decode( $data );
+        return $data ;
+    }
+
+
+    public function getConversions( $params = array() ){
+        $url = $this->baseUrl.'/api/admin/conversion';
+        $client = new Client();
+
+        $response =  $client->request('GET', $url, [
+            'query' => $params
+        ]);
+        $data = $response->getBody()->getContents();
+        $data = json_decode( $data );
+        return $data ;
+    }
+
+
+}
---
+++ app/Services/GoUlu.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Services;
+use GuzzleHttp\Client;
+
+
+class GoUlu{
+    protected $baseUrl;
+
+    public function __construct()
+    {
+        $this->baseUrl = 'http://go.ulu.vn';
+    }
+
+    public function createShortLink( $token, $params  ){
+        $url = $this->baseUrl.'/api/url';
+
+        $client = new Client();
+
+        $response =  $client->request('POST', $url, [
+            'headers' => [
+                'authorization' => 'Bearer '.$token,
+            ],
+            'json' => $params
+        ]);
+        $data = $response->getBody()->getContents();
+        $data = json_decode( $data );
+        return $data ;
+    }
+
+
+    public function getLinkHistory( $token, $params  ){
+        $url = $this->baseUrl.'/api/url';
+
+        $client = new Client();
+
+        $response =  $client->request('GET', $url, [
+            'headers' => [
+                'authorization' => 'Bearer '.$token,
+            ],
+            'query' => $params
+        ]);
+        $data = $response->getBody()->getContents();
+        $data = json_decode( $data );
+        return $data ;
+    }
+
+
+
+
+    public function loginAffiliate( $email, $password ){
+        $url = $this->baseUrl.'/api/affiliate/login';
+
+        $client = new Client();
+        $requestData = [
+            'email' => $email,
+            'password' => $password
+        ];
+
+        $response =  $client->request('POST', $url, ['json' => $requestData]);
+        $data = $response->getBody()->getContents();
+        $data = json_decode( $data );
+        return $data ;
+    }
+
+    public function getClickTracking( $token, $params = array() ){
+        $url = $this->baseUrl.'/api/click';
+        $client = new Client();
+
+        $response =  $client->request('GET', $url, [
+            'headers' => [
+                'authorization' => 'Bearer '.$token,
+            ],
+            'query' => $params
+        ]);
+        $data = $response->getBody()->getContents();
+        $data = json_decode( $data );
+        return $data ;
+    }
+
+
+    public function getConversions( $token, $params = array() ){
+        $url = $this->baseUrl.'/api/conversion';
+        $client = new Client();
+
+        $response =  $client->request('GET', $url, [
+            'headers' => [
+                'authorization' => 'Bearer '.$token,
+            ],
+            'query' => $params
+        ]);
+        $data = $response->getBody()->getContents();
+        $data = json_decode( $data );
+        return $data ;
+    }
+
+
+}
